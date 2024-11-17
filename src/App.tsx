877b28742b4df33ecdcf5b0aefff0c5c90bde7c3
Jon Naske <jonnaske@hotmail.com>
2024-11-17 09:12:20 -0800

    refactor App component to use Card and Slider for prime number input and display

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,65 @@
 import { useEffect, useState } from "react"
-import reactLogo from "./assets/react.svg"
-import viteLogo from "/vite.svg"
 import { Input } from "./components/ui/input"
+import { isPrime } from "./lib/prime"
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "./components/ui/card"
+import { Slider } from "./components/ui/slider"
+import { Github } from "lucide-react"
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [currentNumber, setCurrentNumber] = useState(0)
   const [isPrimeNumber, setIsPrimeNumber] = useState(false)
 
-  function isPrime(num: number): boolean {
-    if (num <= 1) return false
-    if (num <= 3) return true
-    if (num % 2 === 0 || num % 3 === 0) return false
-    for (let i = 5; i * i <= num; i += 6) {
-      if (num % i === 0 || num % (i + 2) === 0) return false
-    }
-    return true
-  }
-
   useEffect(() => {
-    setIsPrimeNumber(isPrime(count))
-  }, [count])
+    setIsPrimeNumber(isPrime(currentNumber))
+  }, [currentNumber])
 
   return (
-    <main
-      data-theme="dark"
-      className="dark grid w-full h-screen place-items-center gap-4"
-    >
-      <h1 className="text-6xl">Prime numbers</h1>
-      <p>A prime number is a number that can only be divided by 1 or itself</p>
-      <Input
-        type="number"
-        placeholder="Enter a number"
-        onChange={e => setCount(Number(e.target.value))}
-      />
-      <p>
-        {count} is {isPrimeNumber ? "a prime number" : "not a prime number"}
-      </p>
-    </main>
+    <>
+      <header className="fixed bg-transparent text-foreground dark place-items-end grid w-full h-8">
+        <a href="https://github.com/j-nas/prime">
+          <Github className="mr-4" />
+        </a>
+      </header>
+      <main className="bg-background text-foreground dark grid w-full h-screen place-items-center gap-4">
+        <Card>
+          <CardHeader>
+            <CardTitle>Prime Numbers</CardTitle>
+            <CardDescription>
+              A prime number is a number that can only be divided by 1 or itself
+            </CardDescription>
+          </CardHeader>
+          <CardContent>
+            <div className="grid gap-4">
+              <Slider
+                defaultValue={[0]}
+                max={1000}
+                step={1}
+                value={[currentNumber]}
+                onValueChange={e => setCurrentNumber(e[0])}
+                className={`${isPrimeNumber && "text-green-600"}`}
+              />
+              <Input
+                type="number"
+                inputMode="numeric"
+                placeholder="Enter a number"
+                onChange={e => setCurrentNumber(Number(e.target.value))}
+                className={`${isPrimeNumber && "ring-green-600 ring"}`}
+              />
+              <p className={`${isPrimeNumber && "text-green-600"}`}>
+                {currentNumber} is{" "}
+                {isPrimeNumber ? "a prime number ✅" : "not a prime number ☠"}
+              </p>
+            </div>
+          </CardContent>
+        </Card>
+      </main>
+    </>
   )
 }
 
